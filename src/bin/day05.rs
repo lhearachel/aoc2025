d329e55921afc98ac095ba8ee14bdb3af95fb289
Rachel <lhearachel@proton.me>
2025-12-05 15:51:39 -0800

    Day 5 solution

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -9,18 +9,45 @@ fn main() {
     println!("Part 2: {}", part_two(&fdata));
 }
 
-fn parse(input: &str) -> () {
-    todo!()
+fn parse(input: &str) -> (Vec<(usize, usize)>, Vec<usize>) {
+    let (lines_fresh, lines_ingredients) = input.split_once("\n\n").unwrap();
+
+    let mut fresh = lines_fresh.lines()
+        .map(|line| {
+            let (head, tail) = line.split_once('-').unwrap();
+            (head.parse().unwrap(), tail.parse().unwrap())
+        })
+        .collect::<Vec<(usize, usize)>>();
+    fresh.sort();
+
+    let mut merged = Vec::from([fresh[0]]);
+    for &(h1, t1) in &fresh[1..] {
+        let &(h2, t2) = merged.last().unwrap();
+        if h1 > t2 {
+            merged.push((h1, t1));
+        } else {
+            *merged.last_mut().unwrap() = (h2, t2.max(t1));
+        }
+    }
+
+    let ingredients = lines_ingredients.lines()
+        .map(|i| i.parse().unwrap())
+        .collect::<Vec<usize>>();
+
+    (merged, ingredients)
 }
 
-fn part_one(input: &str) -> u32 {
-    parse(input);
-    0
+fn part_one(input: &str) -> usize {
+    let (fresh, ingredients) = parse(input);
+
+    ingredients.iter()
+        .filter(|i| fresh.iter().any(|(h, t)| h <= i && t >= i))
+        .count()
 }
 
-fn part_two(input: &str) -> u32 {
-    parse(input);
-    0
+fn part_two(input: &str) -> usize {
+    let (fresh, _) = parse(input);
+    fresh.iter().map(|(h, t)| t - h + 1).sum()
 }
 
 #[cfg(test)]
@@ -29,11 +56,11 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        assert_eq!(part_one(&aoc::load_file("./examples", DAY)), 0);
+        assert_eq!(part_one(&aoc::load_file("./examples", DAY)), 3);
     }
 
     #[test]
     fn test_part_two() {
-        assert_eq!(part_two(&aoc::load_file("./examples", DAY)), 0);
+        assert_eq!(part_two(&aoc::load_file("./examples", DAY)), 14);
     }
 }
